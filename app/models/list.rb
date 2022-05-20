ed065976e407829e050ed159f48e6081985af7e1
vitaliy <vitaliy.skoryy@googlemail.com>
2022-05-20 17:21:36 +0200

    working

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,15 +1,8 @@
 class List < ApplicationRecord
-  validates :name, presence: true
-  validates_uniqueness_of :name
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
+  has_many :reviews, dependent: :destroy
+  has_one_attached :photo
 
-  # def connect
-  #   create_table :assignments do |t|
-  #     t.references :city, index: true, foreign_key: true
-  #     t.references :cleaner, index: true, foreign_key: true
-
-  #     t.timestamps null: false
-  #   end
-  # end
+  validates :name, uniqueness: true, presence: true
 end
